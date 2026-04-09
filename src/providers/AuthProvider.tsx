c43e192747125a2de1a7b59c6066d4ef0298ecbb
Yekaterina <85305642+Yekaterina-Belaya@users.noreply.github.com>
2026-04-09 15:54:52 +0400

    fix

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,5 +1,5 @@
 import { TUser } from '@/types/user';
-import { createContext, useState, useEffect, ReactNode } from 'react';
+import { createContext, useState, useEffect, ReactNode, FC } from 'react';
 
 type TAuthContext = {
   user: TUser | null;
@@ -8,9 +8,13 @@ type TAuthContext = {
   logout: () => void;
 };
 
+type TPropWithChildren = {
+  children: ReactNode
+}
+
 export const AuthContext = createContext<TAuthContext | undefined>(undefined);
 
-export const AuthProvider = ({ children }: { children: ReactNode }) => {
+export const AuthProvider: FC<TPropWithChildren> = ({ children }) => {
   const [user, setUser] = useState<any | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
